01e7188ee8a15c9b8b0f885eaab2a6b7deb31562
Lal-jeet <laljeetofficialgithub@gmail.com>
2025-06-10 12:36:49 +0530

    Add files via upload

---
+++ Program_2.java
@@ -0,0 +1,18 @@
+package com.stream_API_mcq;
+
+public class Program_2 {
+//	Q2.   What is a Stream in the context of the Java 8 Stream API?
+//
+//			 A) A data structure for storing elements.
+//
+//
+//			 B) A sequence of data elements that can be processed in parallel or sequentially.
+//
+//
+//			 C) A container for holding collections.
+//
+//
+//			 D) A type of exception thrown during runtime.
+}
+
+//  B right option
