4c4008a5a6744331769d1f9a496275bbbb8e258d
Lal-jeet <laljeetofficialgithub@gmail.com>
2025-06-10 12:37:15 +0530

    Add files via upload

---
+++ Program_3.java
@@ -0,0 +1,18 @@
+package com.stream_API_mcq;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Program_3 {
+	public static void main(String[] args) {
+		Comparator<Integer> comparator = new Comparator<Integer>() {
+		public int compare(Integer o1, Integer o2) {
+		return o2.compareTo(o1);
+		}
+		};
+		List<Integer> list = List.of(54,34,234,23,364,3);
+		list.stream().sorted(comparator).filter(k -> k%2 == 1).forEach(k -> System.out.print(k + " "));
+		}
+}
+
+// 23 3 
