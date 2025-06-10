f3431e6faaf9c76c17738a02415384eed27262bd
Lal-jeet <laljeetofficialgithub@gmail.com>
2025-06-10 12:38:26 +0530

    Add files via upload

---
+++ Program_8.java
@@ -0,0 +1,18 @@
+package com.stream_API_mcq;
+
+public class Program_8 {
+//	Q8.   What is the purpose of the collect() method in the Stream API?
+//
+//			 A) To transform the elements of a Stream.
+//
+//
+//			 B) To filter the elements in a Stream based on a predicate.
+//
+//
+//			 C) To perform a reduction operation on the Stream elements.
+//
+//
+//			 D) To accumulate the elements of a Stream into a collection or a summary result.
+}
+
+// D
