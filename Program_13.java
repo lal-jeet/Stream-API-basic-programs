cefe9bd3b110e310cc27f98ee6b9271aecd55f7d
Lal-jeet <laljeetofficialgithub@gmail.com>
2025-06-11 23:38:44 +0530

    Add files via upload

---
+++ Program_13.java
@@ -0,0 +1,22 @@
+package com.stream_API_mcq;
+
+public class Program_13 {
+/*
+ * Q13.   What is the difference between the map() and flatMap() methods in Optional?
+
+
+ a) map() can return null, flatMap() cannot
+
+
+ b) map() returns an Optional, flatMap() returns the value directly
+
+
+ c) flatMap() flattens nested Optionals, map() does not
+
+
+ d) There is no difference
+ */
+}
+
+
+// C
