e29910015cbaf63d3ae89702e6564ab9a663e7fe
Lal-jeet <laljeetofficialgithub@gmail.com>
2025-06-11 23:38:58 +0530

    Add files via upload

---
+++ Program_14.java
@@ -0,0 +1,22 @@
+package com.stream_API_mcq;
+
+public class Program_14 {
+
+	/*
+	 Q14.   Which method is used to create a Stream from a collection in Java 8?
+
+
+ A) getStream()
+
+
+ B) parallelStream()
+
+
+ c) stream()
+
+
+ D) createStream()
+	 */
+}
+
+// C
