0e14947a1b0602f73560712e3bc4de029252f701
Lal-jeet <laljeetofficialgithub@gmail.com>
2025-06-11 23:40:06 +0530

    Add files via upload

---
+++ Program_19.java
@@ -0,0 +1,21 @@
+package com.stream_API_mcq;
+
+public class Program_19 {
+	/*
+	 * 
+	Q19.   What is the purpose of the `peek` method in the Stream API?
+
+	 A) It is used to modify the elements of the stream
+
+
+	 B) It is used to perform an action without interfering with the elements
+
+
+	 C) It is used to skip elements in the stream
+
+
+	 D) It is used to concatenate streams
+	 */
+	}
+
+	// B
