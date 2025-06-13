b05de854c0574c11b68c8ebd8102c63f2f0f2981
Lal-jeet <laljeetofficialgithub@gmail.com>
2025-06-13 23:24:38 +0530

    Add files via upload

---
+++ Program_23.java
@@ -0,0 +1,21 @@
+package com.stream_API_mcq;
+
+public class Program_23 {
+	/*
+	 * What happens when you call get() on an empty Optional object?
+
+
+	 a) Returns null    .....
+
+
+	 b) Returns a default value
+
+
+	 c) An NoSuchElementException is thrown
+
+
+	 d) An OptionalException is thrown
+	 */
+	}
+
+	//A
