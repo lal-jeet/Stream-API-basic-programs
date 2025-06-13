859d5fd7490eff203cbb79cb3d44409fb6555d4a
Lal-jeet <laljeetofficialgithub@gmail.com>
2025-06-13 23:25:09 +0530

    Add files via upload

---
+++ Program_25.java
@@ -0,0 +1,20 @@
+package com.stream_API_mcq;
+
+public class Program_25 {
+/*
+ * Which is the new method introduced in java 8 to iterate over a collection?
+
+ a) for (String i : StringList)
+
+
+ b) foreach (String i : StringList)    ...............
+
+
+ c) stringList.forEach()
+
+
+ d) List.for()
+ */
+}
+
+// B
