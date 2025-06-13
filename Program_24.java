0fb675967592ec4b60d6f43161b0fac6d9dbb07c
Lal-jeet <laljeetofficialgithub@gmail.com>
2025-06-13 23:24:54 +0530

    Add files via upload

---
+++ Program_24.java
@@ -0,0 +1,21 @@
+package com.stream_API_mcq;
+
+import java.util.ArrayDeque;
+
+public class Program_24 {
+	public static void main(String[] args) {
+		ArrayDeque<String> de = new ArrayDeque<String>();
+		de.push("well !!");
+		de.push("prepare");
+		System.out.println("Hi Everyone All The Best!!!");
+		for (Object element : de) {
+		System.out.println(element);
+		}
+		}
+}
+
+/*
+Hi Everyone All The Best!!!
+prepare
+well !!
+*/
