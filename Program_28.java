3afbd8c587988534c16274c9431b504dcb189d0c
Lal-jeet <laljeetofficialgithub@gmail.com>
2025-06-15 23:36:57 +0530

    Add files via upload

---
+++ Program_28.java
@@ -0,0 +1,20 @@
+package com.stream_API_mcq;
+
+public class Program_28 {
+/*
+ * What is the purpose of the Optional class in Java?
+
+ A) To handle null values more gracefully
+
+
+ B) To create immutable objects
+
+
+ C) To manage thread synchronization
+
+
+ D) To optimize memory usage
+ */
+}
+
+//A
